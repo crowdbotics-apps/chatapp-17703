d06d1ab4d678d29ba88667e02b7f52613ab567fe
Crowdbotics <team@crowdbotics.com>
2020-06-02 13:41:43 +0000

    Update models

--- backend/chatapp_17703/wsgi.py
+++ backend/chatapp_17703/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatapp_17703.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatapp_17703.settings")
 
 application = get_wsgi_application()
